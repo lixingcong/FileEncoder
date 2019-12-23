02deeecd88d68c0060ea0f8bbb1159126705acaa
lixingcong <lixingcong@live.com>
2019-12-23 20:18:23 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * %{Cpp:License:FileName}
+ *
+ * FIXME: 功能简要概述
+ *
+ * Created on: 2019年 12月 23日
+ * Author: lixingcong
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
