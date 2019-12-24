1ab46ea417b8e2571345ae931e777713ff096d24
lixingcong <lixingcong@live.com>
2019-12-24 10:50:18 +0800

    handle line commands

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 #include <iconv.h>
 #include <cstring>
 #include <fstream>
+#include <regex>
+#include <vector>
+#include <sstream>
 
 #if defined(_MSC_VER) && (_MSC_VER > 1600)
 #pragma execution_character_set("utf-8")
@@ -46,24 +49,82 @@ std::string myIconv(const char* fromCharset, const char* toCharset, const std::s
 	return destString;
 }
 
+// C++11 std::ECMAScript syntax
+std::string myRegex(const char* findRegex, const char* replaceTo, const std::string& str)
+{
+	std::regex re(findRegex);
+	return std::regex_replace(str, re, replaceTo, std::regex_constants::format_default);
+}
+
+void printHelp(const char* exe)
+{
+	std::cout << exe << " SRC DEST SRC_CHARSET DEST_CHARSET [REGEX REPLACE_TO]" << std::endl;
+	std::cout << "\t" << "[REGEX REPLACE_TO] should be a array, or could be empty" << std::endl;
+	std::cout << "\t\t" << "abc:xyz;123:456" << std::endl;
+	std::cout << "\t\t" << "it means find 'abc' then replace to 'xyz', and find '123' then replace to '456'" << std::endl;
+	std::cout << std::endl;
+	std::cout << "\t" << "full example:" << std::endl;
+	std::cout << "\t" << exe << " /tmp/src.txt /tmp/dest.txt gbk2312 utf-8 123:567;ABC:DEF" << std::endl;
+	std::cout << "\t" << exe << " /tmp/src.txt /tmp/dest.txt gbk2312 utf-8" << std::endl;
+}
+
+struct ReplacePair
+{
+	std::string findRegex;
+	std::string replaceTo;
+};
+
+bool ifFileExisted(const char* filename) {
+	std::ifstream f(filename);
+	return f.good();
+}
+
 int main(int argc, char** argv)
 {
-#if defined(_MSC_VER)
-	std::ifstream ifs("Z:\\gbk.txt");
-#else
-	std::ifstream ifs("/tmp/gbk.txt");
-#endif
+	// Retrieve the (non-option) argument:
+	if ((argc < 5) || (argv[argc - 1] == nullptr)) {  // there is NO input...
+		printHelp(argv[0]);
+		return 1;
+	}
+
+	const char* srcFileName = argv[1];
+	const char* destFileName = argv[2];
+	const char* fromCharset = argv[3];
+	const char* toCharset = argv[4];
+	std::vector<ReplacePair> replaceStrPairs;
+
+	if (6 == argc){
+		std::string replaceParams(argv[5]);
+		std::replace(replaceParams.begin(), replaceParams.end(), ';', ' ');  // replace ';' by ' '
+		std::stringstream ss1(replaceParams);
+		std::string replacement;
+		while (ss1 >> replacement){
+			std::replace(replacement.begin(), replacement.end(), ':', ' ');  // replace ':' by ' '
+			std::stringstream ss2(replacement);
+
+			ReplacePair p;
+			ss2 >> p.findRegex >> p.replaceTo;
+			std::cout << "FFF: " << p.findRegex << ",    " << p.replaceTo << std::endl;
+			replaceStrPairs.push_back(p);
+		}
+	}
+
+	if (!ifFileExisted(srcFileName)){
+		std::cerr << "Open " << srcFileName << " FAIL" << std::endl;
+		return 1;
+	}
+
+	std::ifstream ifs(srcFileName);
 	std::string   inContent((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
 
-	std::string outContent = myIconv("gb2312", "utf-8", inContent);
-	//std::cout << myIconv("utf-8", "gb2312", content) << std::endl;
-	//std::cout << myIconv("gb2312", "utf-8", content) << std::endl;
+	std::string outContent = myIconv(fromCharset, toCharset, inContent);
 
-#if defined(_MSC_VER)
-	std::ofstream ofs("Z:\\result.txt");
-#else
-	std::ofstream ofs("/tmp/result.txt");
-#endif
+	for (auto it = replaceStrPairs.begin(); it != replaceStrPairs.end(); ++it){
+		outContent = myRegex((*it).findRegex.c_str(), (*it).replaceTo.c_str(), outContent);
+	}
+	//outContent = myRegex("(STR)(\\(\".*?\"\\))", "FUCK$2", outContent);
+
+	std::ofstream ofs(destFileName);
 	ofs << outContent;
 
 	return 0;
